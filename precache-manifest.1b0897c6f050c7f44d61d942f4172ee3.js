c8f84423facfe8d18fcac88853538ad5e06eedc4
ajanthan <ajanthan.kt@gmail.com>
2020-05-09 17:16:27 +1000

    Updates

---
+++ precache-manifest.1b0897c6f050c7f44d61d942f4172ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35754b87351176b34d033764fa73cef3",
+    "url": "/computervisionclient/index.html"
+  },
+  {
+    "revision": "3f538c7a735d33b7b945",
+    "url": "/computervisionclient/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "cc380cd8b2fcbaf02238",
+    "url": "/computervisionclient/static/js/2.af05d1db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/computervisionclient/static/js/2.af05d1db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f538c7a735d33b7b945",
+    "url": "/computervisionclient/static/js/main.c95b3c99.chunk.js"
+  },
+  {
+    "revision": "1a55c24b4f1e138f4938",
+    "url": "/computervisionclient/static/js/runtime-main.b35a1149.js"
+  }
+]);
